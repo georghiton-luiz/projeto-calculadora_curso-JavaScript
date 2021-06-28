c636324a36d5b31e5fb1d317add662fb82c1dd8b
georghiton-luiz <georghiton@hotmail.com>
2021-06-28 11:57:35 -0300

    Implementação do metodo Date

--- assets/js/controller/CalcController.js
+++ assets/js/controller/CalcController.js
@@ -1,22 +1,60 @@
 class CalcController{
     constructor(){
-        this._displayCalc = "0";
-        this._dataAtual;        
+        this._locale = 'pt-BR';
+        this._diplayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate; 
+        this.initialize();       
+    }
+
+    initialize(){
+
+        this.setDisplayDateTime();
+
+        setInterval(() => {
+            this.setDisplayDateTime() ;
+        }, 1000)
+    }
+
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+        this._timeEl.innerHTML = value
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        this._dateEl.innerHTML = value
     }
 
     get displayCalc(){
-        return this._displayCalc;
+        return this._diplayCalcEl.innerHTML;
     }
 
-    set displayCalc(valor){
-        this._displayCalc = valor
+    set displayCalc(value){
+        this._diplayCalcEl.innerHTML = value
     }
 
-    get dataAtual(){
-        return this._dataAtual;
+    get currentDate(){
+        return new Date();
     }
 
-    set dataAtual(valor){
-        this._dataAtual = valor
+    set currentDate(value){
+        this._currentDate = value
     }
 }
