8d911499bd343fa4746557bdc056629cee4766e7
georghiton-luiz <georghiton@hotmail.com>
2021-07-05 12:44:23 -0300

    Limitando a quantidade de digitos no display

--- assets/js/controller/CalcController.js
+++ assets/js/controller/CalcController.js
@@ -326,6 +326,10 @@ class CalcController{
     }
 
     set displayCalc(value){
+        if(value.toString().length > 10){
+            this.setError();
+            return false;
+        }
         this._diplayCalcEl.innerHTML = value
     }
 
